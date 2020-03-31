40e76c3b5a21281ac47d109355abfb8d6357c862
Steven Tan <steven@steventan.com.au>
2020-03-31 23:34:52 +1100

    add version number to footer

--- src/components/app/Counter.js
+++ src/components/app/Counter.js
@@ -4,8 +4,8 @@ const Logo = () => {
   let totalSurveys = 976
 
   return (
-    <h2 class="logo-ticker">
-      <span class="logo-ticker__num">
+    <h2 className="logo-ticker">
+      <span className="logo-ticker__num">
         <strong>{totalSurveys}</strong> people have already<br />completed the Symptom Tracker.
       </span>
     </h2>
--- src/components/app/Footer.js
+++ src/components/app/Footer.js
@@ -4,14 +4,15 @@ import ImgArqGroup from "../../assets/img/logos/arq-group.png"
 import ImgSwinburne from "../../assets/img/logos/swinburne-logo.gif"
 
 const Footer = () => {
+  const versionNumber = process.env.REACT_APP_VERSION
   return (
     <div className="container has-text-centered">
       <div className="content is-size-7">
         This website has been developed by a team of public
         health and astrophysics/IT researchers and staff at
-        <a href="https://www.swinburne.edu.au/" target="_blank" class="has-text-dark has-text-weight-bold">
+        <a href="https://www.swinburne.edu.au/" target="_blank" className="has-text-dark has-text-weight-bold">
           Swinburne University of Technology</a>
-          in partnership with <a href="https://arq.group" target="_blank" class="has-text-dark has-text-weight-bold">Arq Group</a>,
+          in partnership with <a href="https://arq.group" target="_blank" className="has-text-dark has-text-weight-bold">Arq Group</a>,
           Australia and the COVID-19 Hackathon and marketing volunteers.
           This will help determine the current and emerging COVID-19 symptoms of
           people across the world. This will help you improve your understanding of your health,
@@ -27,6 +28,8 @@ const Footer = () => {
       <a href="https://arq.group/" target="_blank">
         <img src={ImgSwinburne} alt="Arq Group Logo" />
       </a>
+
+      <h2>{versionNumber}</h2>
     </div>
   )
 }
