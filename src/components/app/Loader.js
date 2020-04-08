dcb72f6ba0f27b12aec8cc6bd39addc0dcc70d67
Steven Tan <steven@steventan.com.au>
2020-04-08 21:29:22 +1000

    fix className issue

--- src/components/app/Loader.js
+++ src/components/app/Loader.js
@@ -3,7 +3,7 @@ import React from "react"
 const Loader = () => {
   return (
     <div className="loader container">
-      <div class="spinner" />
+      <div className="spinner" />
     </div>
   )
 }
