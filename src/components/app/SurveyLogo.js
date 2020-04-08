b4a06fb93e8a9f28647478e3fd824fbe4498f85e
Steven Tan <steven@steventan.com.au>
2020-04-08 22:30:44 +1000

    use alternate header for survey

---
+++ src/components/app/SurveyLogo.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const SurveyLogo = () => {
+  return (
+    <h1 className="logo-text">
+      <span className="logo-text__beat">#BEATCOVID-19</span>
+      <span className="logo-text__now">NOW</span>
+    </h1>
+  )
+}
+
+export default SurveyLogo
