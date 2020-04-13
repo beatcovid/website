64bae46935d38997ea4e7eacf73132f10216b278
Nik Cubrilovic <nc9@protonmail.com>
2020-04-13 13:29:11 +1000

    Switch to markdown parser that supports GitHub markdown

--- src/pages/InformationPage.js
+++ src/pages/InformationPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import ReactMarkdown from "react-markdown"
+import MarkdownContent from "../utils/markdownParser"
 
 const markdownSource =
   "https://raw.githubusercontent.com/beatcovid/policies/master/information.md"
@@ -11,17 +11,9 @@ const InformationPage = () => {
     fetch(markdownSource)
       .then(response => response.text())
       .then(text => setInputContent(text))
-  })
+  }, [])
 
-  return (
-    <div className="container content-page">
-      <div className="columns">
-        <section className="column">
-          {inputContent && <ReactMarkdown source={inputContent} />}
-        </section>
-      </div>
-    </div>
-  )
+  return <MarkdownContent sourceText={inputContent} />
 }
 
 export default InformationPage
---
+++ src/utils/markdownParser.js
@@ -0,0 +1,31 @@
+import React from "react"
+import showdown from "showdown"
+
+const parseMarkdown = sourceText => {
+  // bunch of options at https://github.com/showdownjs/showdown
+  const converter = new showdown.Converter()
+  converter.setFlavor("github")
+
+  const htmlContent = converter.makeHtml(sourceText)
+
+  return htmlContent
+}
+
+const MarkdownContent = ({ sourceText }) => {
+  const htmlContent =
+    typeof sourceText === "string" ? parseMarkdown(sourceText) : undefined
+
+  return (
+    <div className="container content-page">
+      <div className="columns">
+        <section className="column">
+          {htmlContent && (
+            <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+          )}
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default MarkdownContent
