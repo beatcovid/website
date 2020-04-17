85d34c3647693f3b83393d262075a31e83652f7a
Nik Cubrilovic <nc9@protonmail.com>
2020-04-18 09:42:24 +1000

    BEAT-186 Remove fingerprintjs completely as we're not using it

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
-import Fingerprint2 from "fingerprintjs2"
 import * as Sentry from "@sentry/browser"
 import App from "./App"
 import store from "./store"
 import * as serviceWorker from "./utils/serviceWorker"
 import "./assets/styles/main.scss"
-
-Fingerprint2.get({}, function(components) {
-  var values = components.map(function(component) {
-    return component.value
-  })
-  var murmur = Fingerprint2.x64hash128(values.join(""), 31)
-  console.log("Fingerprint", murmur)
-})
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
