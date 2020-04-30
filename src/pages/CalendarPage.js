099670a1d74775d5e3a78d5a549398484e3cce01
Steven Tan <steven@steventan.com.au>
2020-04-30 10:04:33 +1000

    refactor calendar page

--- src/pages/CalendarPage.js
+++ src/pages/CalendarPage.js
@@ -1,55 +1,26 @@
-import React, { useState, useEffect, useRef } from "react"
-import format from "date-fns/format"
-import isValid from "date-fns/isValid"
-import eachMonthOfInterval from "date-fns/eachMonthOfInterval"
-import addMonths from "date-fns/addMonths"
-import { Month, DaysOfWeek } from "../components/calendar"
+import React, { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import Calendar from "../components/calendar"
+import {
+  doTrackerGet,
+  selectTracker,
+  selectIsTrackerError,
+} from "../store/userSlice"
 
 const CalendarPage = () => {
-  const now = new Date()
-  const calendarMonthsRef = useRef(null)
-  const startOfYear = new Date(2020, 0, 1) // nothing is collected before 2020
-  const months = eachMonthOfInterval({
-    start: startOfYear,
-    end: addMonths(now, 1),
-  })
-  const headerDates = {}
-  const [currentHeaderDate, setCurrentHeaderDate] = useState(null)
+  const dispatch = useDispatch()
+  const tracker = useSelector(selectTracker)
+  const isTrackerError = useSelector(selectIsTrackerError)
 
   useEffect(() => {
-    calendarMonthsRef.current.scrollTo(
-      0,
-      calendarMonthsRef.current.scrollHeight,
-    )
-  }, [])
-
-  function handleIntersect(date, isIntersecting) {
-    headerDates[date] = isIntersecting
-    const visibleDates = []
-    Object.keys(headerDates).forEach(d => {
-      if (headerDates[d]) {
-        visibleDates.push(new Date(d))
-      }
-    })
-    const current = new Date(Math.min(...visibleDates))
-    const currentHeader = isValid(current) ? format(current, "LLLL yyyy") : ""
-    setCurrentHeaderDate(currentHeader)
-  }
-
-  function renderCalendarMonth(date) {
-    const key = `calendar-page-${date.getTime()}`
-    return <Month key={key} date={date} onIntersect={handleIntersect} />
-  }
+    if (!tracker && !isTrackerError) {
+      dispatch(doTrackerGet())
+    }
+  }, [dispatch, tracker, isTrackerError])
 
   return (
     <section className="calendar-page container">
-      <header className="calendar-header">
-        <h1>{currentHeaderDate}</h1>
-        <DaysOfWeek week={months[0]} />
-      </header>
-      <div ref={calendarMonthsRef} className="calendar-months">
-        {months.map(renderCalendarMonth)}
-      </div>
+      <Calendar />
     </section>
   )
 }
