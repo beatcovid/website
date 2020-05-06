84b5681c3def5eb87cef63c426816ecf01d5e344
Nik Cubrilovic <nc9@protonmail.com>
2020-05-07 02:48:41 +1000

    Markdown and HTML formatters

--- src/lib/intl/FormattedMessage.js
+++ src/lib/intl/FormattedMessage.js
@@ -1,16 +1,48 @@
-import { useIntl } from "react-intl"
+import React, { renderToStaticMarkup } from "react"
+import { useIntl, FormattedMessage } from "react-intl"
 import messages from "./messages"
+import { parseMarkdown } from "../../utils/markdownParser"
 
-export default ({ msgId }) => {
+const FormattedMsgId = ({ msgId }) => {
   const intl = useIntl()
 
   let m = Object.keys(messages).filter(m => messages[m].id === msgId)
 
   if (m.length < 1) {
+    console.info(`Could not find translation for msgId ${msgId}`)
     return null
   }
 
   let keyId = m.pop()
 
+  console.info(`making translation for key ${keyId}`)
+
   return intl.formatMessage(messages[keyId])
 }
+
+export const HtmlFormattedText = props => (
+  <FormattedMessage
+    {...props}
+    values={{
+      b: (...parts) => <b>{parts}</b>,
+      strong: (...parts) => <strong>{parts}</strong>,
+      p: (...parts) => <p>{parts}</p>,
+      i: (...parts) => <i>{parts}</i>,
+    }}
+  />
+)
+
+export const MarkdownFormattedText = ({ id, defaultMessage }) => {
+  const intl = useIntl()
+
+  const translated = intl.formatMessage({
+    id,
+    defaultMessage,
+  })
+
+  const htmlContent = parseMarkdown(translated)
+
+  return <span dangerouslySetInnerHTML={{ __html: htmlContent }} />
+}
+
+export { FormattedMessage }
