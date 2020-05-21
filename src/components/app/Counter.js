a4e57d32f891b19d7319264275ffd8ebce603a68
Nik Cubrilovic <nc9@protonmail.com>
2020-05-22 02:33:31 +1000

    Extract counter message for intl

--- src/components/app/Counter.js
+++ src/components/app/Counter.js
@@ -1,17 +1,23 @@
 import React from "react"
 import numeral from "numeral"
+import { FormattedMessage } from "react-intl"
 
-const Counter = props => {
-  const count = props.count
+const Counter = ({ count }) => {
   const countFormatted = numeral(count).format("0,0")
 
   return (
     <h2 className="logo-ticker">
       {count && count > 0 && (
         <span className="logo-ticker__num">
-          <strong>{countFormatted}</strong> people are helping
-          <br />
-          to #BeatCovid19Now
+          <FormattedMessage
+            id="web.counter"
+            defaultMessage="<strong>{count}</strong> people are helping{br} to #BeatCovid19Now"
+            values={{
+              strong: (...p) => <strong>{p}</strong>,
+              br: <br />,
+              count: countFormatted,
+            }}
+          />
         </span>
       )}
     </h2>
