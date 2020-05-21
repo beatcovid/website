15ab81a19bc27cccf18807e0cabd9f7569c22af9
Nik Cubrilovic <nc9@protonmail.com>
2020-05-22 03:08:13 +1000

    Extract contacts for intl

--- src/components/app/Contacts.js
+++ src/components/app/Contacts.js
@@ -1,34 +1,37 @@
 import React from "react"
+import { FormattedMessage } from "react-intl"
 
 const Contacts = () => {
   return (
     <footer className="site-footer contacts-section container">
-      <p>
-        We do not expect that completing the Symptom Tracker will cause you any
-        distress. However, if you are experiencing distress, please contact{" "}
-        <a href="https://lifeline.org.au" aria-label="Lifeline website">
-          Lifeline
-        </a>{" "}
-        or your local relevant crisis help line.
-      </p>
-      <p>
-        If you have any questions about the Symptom Tracker, please contact the
-        Chief Investigator at the{" "}
-        <a
-          href="https://www.swinburne.edu.au/research/global-health-equity/"
-          aria-label="Link to Centre for Global Health and Equity"
-        >
-          Centre for Global Health and Equity
-        </a>
-        ,{" "}
-        <a
-          href="mailto:beatcovid19now@swin.edu.au"
-          aria-label="Email Professor Richard Osborne"
-        >
-          Professor Richard Osborne
-        </a>
-        .
-      </p>
+      <FormattedMessage
+        id="web.footer.contacts"
+        defaultMessage="<p>We do not expect that completing the Symptom Tracker will cause you any distress. However, if you are experiencing distress, please contact <lifeline>Lifeline</lifeline> or your local relevant crisis help line.</p><p>If you have any questions about the Symptom Tracker, please contact the Chief Investigator at the <swinlink>Centre for Global Health and Equity</swinlink>, <richard>Professor Richard Osborne</richard>.</p>"
+        values={{
+          richard: (...p) => (
+            <a
+              href="mailto:beatcovid19now@swin.edu.au"
+              aria-label="Email Professor Richard Osborne"
+            >
+              {p}
+            </a>
+          ),
+          p: (...p) => <p>{p}</p>,
+          swinlink: (...p) => (
+            <a
+              href="https://www.swinburne.edu.au/research/global-health-equity/"
+              aria-label="Link to Centre for Global Health and Equity"
+            >
+              {p}
+            </a>
+          ),
+          lifeline: (...p) => (
+            <a href="https://lifeline.org.au" aria-label="Lifeline website">
+              {p}
+            </a>
+          ),
+        }}
+      />
     </footer>
   )
 }
