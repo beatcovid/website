6ffcbae968c2621ecb4b16fe6363fc1d6356287d
Nik Cubrilovic <nc9@protonmail.com>
2020-05-22 02:42:06 +1000

    Extract acknowledgement string for intl

--- src/components/app/Acknowledgement.js
+++ src/components/app/Acknowledgement.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { FormattedMessage } from "react-intl"
 import ImgNcris from "../../assets/img/logos/ncris.png"
 import ImgArqGroup from "../../assets/img/logos/arq-group.png"
 import ImgSwinburne from "../../assets/img/logos/swinburne-logo.gif"
@@ -7,30 +8,39 @@ const Acknowledgement = () => {
   return (
     <footer className="site-footer container">
       <div className="is-size-7">
-        This website has been developed by a team of public health and
-        astrophysics/IT researchers and staff at{" "}
-        <a
-          href="https://www.swinburne.edu.au/"
-          rel="noopener noreferrer"
-          target="_blank"
-          className="has-text-dark has-text-weight-bold"
-        >
-          Swinburne University of Technology
-        </a>{" "}
-        in partnership with{" "}
-        <a
-          href="https://arq.group"
-          rel="noopener noreferrer"
-          target="_blank"
-          className="has-text-dark has-text-weight-bold"
-        >
-          Arq Group
-        </a>
-        , Australia and the COVID-19 Hackathon and marketing volunteers. This
-        will help determine the current and emerging COVID-19 symptoms of people
-        across the world. This will help you improve your understanding of your
-        health, follow changes and help us to advise health authorities about
-        what’s going on from the community’s perspective.
+        <FormattedMessage
+          id="web.acknowledgement"
+          defaultMessage={`This website has been developed by a team of public health and
+            astrophysics/IT researchers and staff at <swinlink>Swinburne University of
+            Technology</swinlink> in partnership with <arqlink>Arq Group</arqlink>, Australia
+            and the COVID-19 Hackathon and marketing volunteers. This
+            will help determine the current and emerging COVID-19 symptoms of people
+            across the world. This will help you improve your understanding of your
+            health, follow changes and help us to advise health authorities about
+            what’s going on from the community’s perspective.`}
+          values={{
+            swinlink: (...p) => (
+              <a
+                href="https://www.swinburne.edu.au/"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="has-text-dark has-text-weight-bold"
+              >
+                {p}
+              </a>
+            ),
+            arqlink: (...p) => (
+              <a
+                href="https://arq.group"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="has-text-dark has-text-weight-bold"
+              >
+                {p}
+              </a>
+            ),
+          }}
+        />
       </div>
 
       <section className="logos">
