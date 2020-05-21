ff06c6637abcee5a9d57af7c5783240ec63c54b1
Nik Cubrilovic <nc9@protonmail.com>
2020-05-22 02:47:08 +1000

    Extract page not found message for intl

--- src/pages/NoMatchPage.js
+++ src/pages/NoMatchPage.js
@@ -1,16 +1,19 @@
 import React from "react"
+import { FormattedMessage } from "react-intl"
 import { Link } from "react-router-dom"
 
-const NoMatchPage = () => {
-  return (
-    <div className="no-match-page container has-text-centered">
-      <h1>Page not found.</h1>
-      <p>You might have entered the incorrect address.</p>
-      <p>
-        Back to <Link to="/">homepage</Link>.
-      </p>
-    </div>
-  )
-}
+const NoMatchPage = () => (
+  <div className="no-match-page container has-text-centered">
+    <FormattedMessage
+      id="web.notfound"
+      defaultMessage="<h1>Page not found.</h1><p>You might have entered an incorrect address.</p><p>Back to <homelink>homepage</homelink></p>"
+      values={{
+        h1: (...p) => <h1>{p}</h1>,
+        p: (...p) => <p>{p}</p>,
+        homelink: (...p) => <Link to="/">{p}</Link>,
+      }}
+    />
+  </div>
+)
 
 export default NoMatchPage
